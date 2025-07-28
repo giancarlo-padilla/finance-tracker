0c47d24d89b0d043438a4c9fd481be2abef73018
giancarlo-padilla <26507188+giancarlo-padilla@users.noreply.github.com>
2025-07-27 21:29:37 -0400

    Formatting, font styling

--- app.js
+++ app.js
@@ -9,41 +9,36 @@ const updateBtn = document.getElementById("update-totals");
 const addHistory = document.getElementById("add-history");
 const historyContainer = document.querySelector(".history");
 
+let lastTotals = { cash: 0, bank: 0, network: 0, total: 0 };
+
 function updateTotals() {
 	const cash = parseFloat(cashInput.value) || 0;
 	const bank = parseFloat(bankInput.value) || 0;
 	const network = parseFloat(networkInput.value) || 0;
 	const total = cash + bank + network;
-	console.log(cash, bank, network);
 
-	totalElem.textContent = total.toFixed(2);
-	cashElem.textContent = cash.toFixed(2);
-	bankElem.textContent = bank.toFixed(2);
-	networkElem.textContent = network.toFixed(2);
+	totalElem.textContent = total.toFixed();
+	cashElem.textContent = cash.toFixed();
+	bankElem.textContent = bank.toFixed();
+	networkElem.textContent = network.toFixed();
 
 	return { cash, bank, network, total };
 }
 
 updateBtn.addEventListener("click", () => {
-	const { cash, bank, network, total } = updateTotals();
+	lastTotals = updateTotals();
 });
 
 addHistory.addEventListener("click", () => {
-	const now = new Date().toLocaleString();
-	const cash = parseFloat(cashElem.textContent) || 0;
-	const bank = parseFloat(bankElem.textContent) || 0;
-	const network = parseFloat(networkElem.textContent) || 0;
-	const total = parseFloat(totalElem.textContent) || 0;
-
+	const now = new Date().toLocaleDateString();
+	const { cash, bank, network, total } = lastTotals;
 	const entryHTML = `
         <div class="entry">
             <div><strong><p>Date:</p></strong><p>${now}</p></div>
-            <div><strong><p>Cash:</p></strong><p>${cash.toFixed(2)}</p></div>
-            <div><strong><p>Bank:</p></strong><p>${bank.toFixed(2)}</p></div>
-            <div><strong><p>Network:</p></strong><p>${network.toFixed(
-													2
-												)}</p></div>
-            <div><strong><p>Total:</p></strong><p>${total.toFixed(2)}</p></div>
+            <div><strong><p>Cash:</p></strong><div class="entry-number"><p>$</p><p>${cash.toFixed()}</p></div></div>
+            <div><strong><p>Bank:</p></strong><div class="entry-number"><p>$</p><p>${bank.toFixed()}</p></div></div>
+            <div><strong><p>Network:</p></strong><div class="entry-number"><p>$</p><p>${network.toFixed()}</p></div></div>
+            <div><strong><p>Total:</p></strong><div class="entry-number"><p>$</p><p>${total.toFixed()}</p></div></div>
         </div>
     `;
 	historyContainer.insertAdjacentHTML("beforeend", entryHTML);
